7cdf41edbce4b3ec0c39729a7d1f88c4526f5203
vcd8j <myncrafttt.06@gmail.com>
2026-09-17 18:11:41 -0700

    Add files via upload

---
+++ MIDterm/Main.java
@@ -0,0 +1,34 @@
+public class Main{
+   public static void main(String[] args){
+   
+   Movie m1= new Movie();
+   
+   m1.title = "Avengers";
+   m1.genre = "Action";
+   m1.duration = 181;
+   
+   m1.displayInfo();     
+   
+   Movie m2= new Movie();
+   
+   m2.title = "Home Alone";
+   m2.genre = "Comedy";
+   m2.duration = 103;
+   
+   m2.displayInfo();     
+   
+   
+  Movie m3= new Movie();
+   
+   m3.title = "Frozen";
+   m3.genre = "Animation";
+   m3.duration = 102;
+   
+   m3.displayInfo();     
+   
+   
+   
+   
+   }
+
+}
